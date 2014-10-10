b3453f6df77bf6f0cf3e9010742792e6e6a19d15
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2014-10-10 17:26:35 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Decrypting Android Snapchat images 
+
+Python script for decrypting stored images from Snapchat version 5.0.34.nn
+ **The script needs a rooted device and USB debugging turned on**.
+
+##How Snapchat decrypted images in previous versions
+
+The new version of Snapchat (v5.0.34.nn), released on September 23rd, 2014) saves the received images differently than previous versions.
+ Snapchat version 5.0.32.3 saved all encrypted images using the _AES_ cipher in _ECB_ mode with a hardcoded password ('_M02cnQ51Ji97vwT4_') under _/data/data/com.snapchat.android/cache/received\_image\_snaps_ folder. Scripts such as [1] could decrypt successfully images pulled from the device. Other ways to obtain the images include the use of the Xposed Framework and KeepChat [2].
+
+##How Snapchat decrypts images now (as of October 10th, 2014)
+
+The new Snapchat version encrypts images using the _AES_ algorithm in _CBC_ mode. For each received image a new random key and initialization vector (IV) is generated to encrypt it.  This results from the use by the  _ReceivedSnaps_ class  of the default algorithm constructor in _com.snapchat.android.util.crypto.CBCEncryption_  as it does not pass it a specific key and IV to use. When the app is stopped, the keys are saved in an encrypted file named _/data/data/com.snapchat.android/cache/bananas_ in order to be able to perform the decryption next time the app runs. Therefore it is necessary to stop the application before trying to decrypt the '_bananas_' file  to obtain all the keys to decrypt the images (_snaps_) . This file is encrypted using the _AES_ algorithm in _ECB_ mode but the key is predictable since the method that generates it can be found in the class _com.snapchat.android.util.crypto.SlightySecurePreferences_. The key is generated from an _MD5_ hash using the _Android ID_ concatenated to the string '_seems legit..._'.
+The content of the decrypted file is a JSON structure. Under the _snapKeysAndIvs_  field one can find all the keys (the corresponding IVs are also unnecessarily stored) to decrypt the images. Each image key and IV has a _snapID_. This ID is associated to the corresponding image file in the _tcspahn.db_ database file under the table _snapimagesfiles_.
+
+##Proof of concept code to decrypt Snapchat v 5.0.34.nn images
+
+The python script first  stops the Snapchat application to force the storage of decryption keys in the _bananas_ file, it then pulls the images from Snapchat internal storage along with the _bananas_ cache file.  Next  it decrypts the last file with the predictable key to obtain the keys and IVs to decrypt the images. After that, it tries to decrypt each image with all possible pairs of key and IVs to find a successful decryption (this could be also done by pulling the database file and reading the _snapID_ associations table mentioned earlier).
+
+If the script fails, try opening Snapchat again to make sure the snaps where downloaded and run the script again. 
+
+##References
+
+[1] https://gist.github.com/jamescmartinez/6913761
+
+[2] http://repo.xposed.info/module/com.ramis.keepchat
+
+
+
---
+++ decrypt_photos.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+## Copyright (c) 2014, Fundación Dr. Manuel Sadosky. Todos los derechos reservados.
+##
+## La redistribución y el uso en las formas de código fuente y binario, con o sin
+## modificaciones, están permitidos siempre que se cumplan las siguientes condiciones:
+##
+## 1. Las redistribuciones del código fuente deben conservar el aviso de copyright
+##   anterior, esta lista de condiciones y el siguiente descargo de responsabilidad.
+##
+## 2. Las redistribuciones en formato binario deben reproducir el aviso de copyright
+##  anterior, esta lista de condiciones y la siguiente renuncia en la documentación
+##   y/u otros materiales suministrados con la distribución.
+##
+## ESTE SOFTWARE SE SUMINISTRA POR LA Fundación Dr. Manuel Sadosky ''COMO ESTÁ'' Y CUALQUIER
+## GARANTÍA EXPRESA O IMPLÍCITAS, INCLUYENDO, PERO NO LIMITADO A, LAS GARANTÍAS
+## IMPLÍCITAS DE COMERCIALIZACIÓN Y APTITUD PARA UN PROPÓSITO DETERMINADO SON
+## RECHAZADAS. EN NINGÚN CASO Fundación Dr. Manuel Sadosky SERÁ RESPONSABLE POR NINGÚN
+## DAÑO DIRECTO, INDIRECTO, INCIDENTAL, ESPECIAL, EJEMPLAR O CONSECUENTE (INCLUYENDO,
+## PERO NO LIMITADO A, LA ADQUISICIÓN DE BIENES O SERVICIOS; LA PÉRDIDA DE USO, DE
+## DATOS O DE BENEFICIOS; O INTERRUPCIÓN DE LA ACTIVIDAD EMPRESARIAL) O POR
+## CUALQUIER TEORÍA DE RESPONSABILIDAD, YA SEA POR CONTRATO, RESPONSABILIDAD ESTRICTA
+## O AGRAVIO (INCLUYENDO NEGLIGENCIA O CUALQUIER OTRA CAUSA) QUE SURJA DE CUALQUIER
+## MANERA DEL USO DE ESTE SOFTWARE, INCLUSO SI SE HA ADVERTIDO DE LA POSIBILIDAD DE
+## TALES DAÑOS.
+##
+## Las opiniones y conclusiones contenidas en el software y la documentación son las
+## de los autores y no deben interpretarse como la representación de las políticas
+## oficiales, ya sea expresa o implícita, de Fundación Dr. Manuel Sadosky .
+##
+##  Decifrado de imagenes de Snapchat version 5.0.34.10
+##  Author: Joaquín Rinaudo
+## 
+
+import subprocess
+import shlex
+import md5
+from hashlib import md5
+from Crypto.Cipher import AES
+from Crypto import Random
+import json
+import time
+import os
+import base64
+import re
+
+def run_get_output(command):
+	try:
+		return subprocess.check_output(shlex.split(command),stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError as grepexc:                                                                                                   
+		return grepexc.output
+
+def run_blocking_cmd(command):
+	return subprocess.call(shlex.split(command),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+def get_android_id():
+	android_id = run_get_output(''' adb shell content query --uri content://settings/secure --projection name:value --where "name=\\'android_id\\'" ''')
+	p = re.compile('Row: 0 name=android_id, value=(.*)')
+	print p.findall(android_id)[0].strip()
+	return p.findall(android_id)[0].strip()
+
+def pull_bananas_cache_file():
+	run_blocking_cmd(''' adb pull /data/data/com.snapchat.android/cache/bananas encrypted_bananas_file ''')
+
+def snapchat_bananas_password():
+	m = md5()
+	m.update( get_android_id() )
+	m.update('seems legit...')
+	return m.hexdigest()
+
+def decrypt_bananas_file():
+	with open("encrypted_bananas_file") as encrypted_bananas:
+		with open("decrypted_bananas_file",'w') as decrypted_bananas:
+			cipher = AES.new(snapchat_bananas_password(), AES.MODE_ECB)
+			decrypt_file(encrypted_bananas,decrypted_bananas,cipher )
+
+def pull_images():
+	run_blocking_cmd(''' adb pull /data/data/com.snapchat.android/cache/received_image_snaps/ encrypted_received_image_snaps/ ''')
+
+def decrypt_images():
+	if not os.path.exists('decrypted_received_image_snaps'): os.makedirs('decrypted_received_image_snaps')
+	with open("decrypted_bananas_file") as json_file:
+		bananas = json.loads(json_file.read().decode('utf8'))
+		if not bananas.get('snapKeysAndIvs',None):
+			print "Images were already viewed, the key and IV were deleted from the /cache/bananas file. Try reopening Snapchat to check if images where downloaded."
+			exit(0)
+		json_bananas = json.loads(bananas['snapKeysAndIvs'],encoding='utf8')
+		if len(json_bananas) < len( os.listdir("encrypted_received_image_snaps") ):
+			print "There are less keys than snaps, some snaps won't be able to be decrypted"
+		for file_name in os.listdir("encrypted_received_image_snaps"):	
+			could_decrypt = False
+			for snap_pair in json_bananas:
+				key= base64.b64decode(json_bananas[snap_pair]['a']).encode('hex') 
+				iv= base64.b64decode( json_bananas[snap_pair]['b']).encode('hex') 
+				s = run_get_output('openssl aes-128-cbc -K %s -iv %s -d -in encrypted_received_image_snaps/%s -out decrypted_received_image_snaps/%s' %(key,iv,file_name,file_name))
+				if s == '':
+					could_decrypt = True
+					break
+					#no error then the image was decoded properly 	
+			if not could_decrypt:
+				print "The image %s could not be decrypted, none of the keys in the bananas file worked" %file_name
+				#break when decrypt work
+
+def decrypt_file(in_file, out_file, cipher):
+    bs = AES.block_size
+    next_chunk = ''
+    finished = False
+    while not finished:
+        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
+        if len(next_chunk) == 0:
+            padding_length = ord(chunk[-1])
+            chunk = chunk[:-padding_length]
+            finished = True
+        out_file.write(chunk)
+
+if __name__ == '__main__':
+	#stop the application to save the 'bananas file'
+	run_blocking_cmd('adb shell am force-stop com.snapchat.android')
+	pull_images()
+	pull_bananas_cache_file()
+	decrypt_bananas_file()
+	decrypt_images()
+	#Cleaning up
+	run_blocking_cmd('rm encrypted_bananas_file decrypted_bananas_file')
+	run_blocking_cmd('rm -r encrypted_received_image_snaps')
